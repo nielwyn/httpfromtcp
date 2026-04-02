605412244b5fef2d9cf0a5ebf80dc07ab63ceeec
nielwyn <nghoangnghiabt@gmail.com>
2026-04-02 13:40:03 +0000

    add HTTP request line parser with streaming support

---
+++ internal/request/request.go
@@ -0,0 +1,123 @@
+package request
+
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+type requestState int
+
+const (
+	requestStateInitialized requestState = iota
+	requestStateParsingHeaders
+	requestStateParsingBody
+	requestStateDone
+)
+
+type Request struct {
+	RequestLine RequestLine
+	// Header      map[string]string
+	// Body        []byte
+	state requestState
+}
+
+type RequestLine struct {
+	HttpVersion   string
+	RequestTarget string
+	Method        string
+}
+
+const crlf = "\r\n"
+const bufferSize = 8
+
+func (r *Request) parse(data []byte) (int, error) {
+	switch r.state {
+	case requestStateInitialized:
+		rl, consumed, err := parseRequestLine(data)
+		if err != nil {
+			return 0, err
+		}
+		if consumed == 0 {
+			return 0, nil
+		}
+		r.RequestLine = *rl
+		r.state = requestStateDone
+		return consumed, nil
+	case requestStateDone:
+		return 0, fmt.Errorf("error: trying to read data in a done state")
+	default:
+		return 0, fmt.Errorf("unknown state: %d", r.state)
+	}
+}
+
+func parseRequestLine(bytes []byte) (*RequestLine, int, error) {
+	str := string(bytes)
+	crlfIndex := strings.Index(str, crlf)
+	if crlfIndex == -1 {
+		return nil, 0, nil
+	}
+
+	numBytesParsed := crlfIndex + len(crlf)
+	requestLineParts := strings.Split(str[:crlfIndex], " ")
+
+	if len(requestLineParts) != 3 {
+		return nil, numBytesParsed, fmt.Errorf("invalid request line")
+	}
+
+	validMethods := map[string]bool{"GET": true, "POST": true, "PUT": true, "DELETE": true, "PATCH": true, "HEAD": true, "OPTIONS": true}
+	if !validMethods[requestLineParts[0]] {
+		return nil, numBytesParsed, fmt.Errorf("invalid method: %s", requestLineParts[0])
+	}
+
+	if !strings.HasPrefix(requestLineParts[2], "HTTP/") {
+		return nil, numBytesParsed, fmt.Errorf("invalid HTTP version: %s", requestLineParts[2])
+	}
+
+	version := strings.TrimPrefix(requestLineParts[2], "HTTP/")
+	validVersions := map[string]bool{"1.1": true}
+	if !validVersions[version] {
+		return nil, numBytesParsed, fmt.Errorf("invalid HTTP version: %s", requestLineParts[2])
+	}
+
+	return &RequestLine{
+		Method:        requestLineParts[0],
+		RequestTarget: requestLineParts[1],
+		HttpVersion:   version,
+	}, numBytesParsed, nil
+}
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	buf := make([]byte, bufferSize)
+	req := &Request{state: requestStateInitialized}
+	readToIndex := 0
+
+	for req.state != requestStateDone {
+		numBytesRead, err := reader.Read(buf[readToIndex:])
+		readToIndex += numBytesRead
+		if readToIndex == cap(buf) {
+			newBuf := make([]byte, cap(buf)*2)
+			copy(newBuf, buf[:readToIndex])
+			buf = newBuf
+		}
+
+		numBytesParsed, parseErr := req.parse(buf[:readToIndex])
+		if parseErr != nil {
+			return nil, parseErr
+		}
+
+		// Shifts the unconsumed bytes to the front of the buffer
+		copy(buf, buf[numBytesParsed:readToIndex])
+		readToIndex -= numBytesParsed
+
+		if err == io.EOF {
+			req.state = requestStateDone
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return req, nil
+}
---
+++ internal/request/request_test.go
@@ -0,0 +1,92 @@
+package request
+
+import (
+	"io"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type chunkReader struct {
+	data            string
+	numBytesPerRead int
+	pos             int
+}
+
+// Read reads up to len(p) or numBytesPerRead bytes from the string per call
+// its useful for simulating reading a variable number of bytes per chunk from a network connection
+func (cr *chunkReader) Read(p []byte) (n int, err error) {
+	if cr.pos >= len(cr.data) {
+		return 0, io.EOF
+	}
+	endIndex := cr.pos + cr.numBytesPerRead
+	endIndex = min(endIndex, len(cr.data))
+	n = copy(p, cr.data[cr.pos:endIndex])
+	cr.pos += n
+
+	return n, nil
+}
+
+func TestRequestLineParse(t *testing.T) {
+	// Test: GET Request line with chunkReader
+	reader := &chunkReader{
+		data:            "GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		numBytesPerRead: 3,
+	}
+	r, err := RequestFromReader(reader)
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
+	// Test: GET Request line with path chunkReader
+	reader = &chunkReader{
+		data:            "GET /pomegranate HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		numBytesPerRead: 1,
+	}
+	r, err = RequestFromReader(reader)
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/pomegranate", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
+	// Test: Good GET Request line
+	r, err = RequestFromReader(strings.NewReader("GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
+	// Test: Good POST Request line with path
+	r, err = RequestFromReader(strings.NewReader("POST /pomegranate HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "POST", r.RequestLine.Method)
+	assert.Equal(t, "/pomegranate", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
+	// Test: Invalid method (out of order) request line
+	r, err = RequestFromReader(strings.NewReader("/ GET HTTP/1.1\r\nHost: localhost:42069\r\n\r\n"))
+	require.Error(t, err)
+	require.Nil(t, r)
+
+	// Test: Invalid version in request line
+	r, err = RequestFromReader(strings.NewReader("GET / HTTP/2.9\r\nHost: localhost:42069\r\n\r\n"))
+	require.Error(t, err)
+	require.Nil(t, r)
+
+	// Test: Invalid method (starts with /)
+	r, err = RequestFromReader(strings.NewReader("/pomegranate HTTP/1.1\r\nHost: localhost:42069\r\n\r\n"))
+	require.Error(t, err)
+	require.Nil(t, r)
+
+	// Test: Missing request target (only 2 parts)
+	r, err = RequestFromReader(strings.NewReader("GET HTTP/1.1\r\nHost: localhost:42069\r\n\r\n"))
+	require.Error(t, err)
+	require.Nil(t, r)
+}
