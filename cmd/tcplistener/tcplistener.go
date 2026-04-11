7dd61e1feeebf14d5aa698a38a1042791cc13bb3
nielwyn <nghoangnghiabt@gmail.com>
2026-04-11 16:07:21 +0000

    add RequestFromReader to tcplistener

--- cmd/tcplistener/tcplistener.go
+++ cmd/tcplistener/tcplistener.go
@@ -2,61 +2,33 @@ package main
 
 import (
 	"fmt"
-	"io"
+	"httpfromtcp/internal/request"
 	"net"
-	"strings"
 )
 
 func main() {
-	l, err := net.Listen("tcp", ":42069")
+	listener, err := net.Listen("tcp", ":42069")
 	if err != nil {
 		fmt.Println(err)
 	}
-	defer l.Close()
+	defer listener.Close()
 
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		go func(c net.Conn) {
-			ch := getLinesChannel(c)
-			for v := range ch {
-				fmt.Println(v)
-			}
-			c.Close()
-		}(conn)
+	conn, err := listener.Accept()
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
-}
+	defer conn.Close()
 
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	var lineBuffer strings.Builder
-	ch := make(chan string)
-	buf := make([]byte, 8)
-	go func() {
-		for {
-			n, err := f.Read(buf)
-			partsSlice := strings.Split(string(buf[:n]), "\n")
+	req, err := request.RequestFromReader(conn)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Request line:\n- Method: %s\n- Target: %s\n- Version: %s\n",
+		req.RequestLine.Method,
+		req.RequestLine.RequestTarget,
+		req.RequestLine.HttpVersion,
+	)
 
-			for i, v := range partsSlice {
-				lineBuffer.WriteString(v)
-				if i == len(partsSlice)-1 {
-					break
-				}
-				ch <- lineBuffer.String()
-				lineBuffer.Reset()
-			}
-
-			if err == io.EOF {
-				if lineBuffer.Len() > 0 {
-					ch <- lineBuffer.String()
-				}
-				f.Close()
-				close(ch)
-				return
-			}
-		}
-	}()
-	return ch
 }
