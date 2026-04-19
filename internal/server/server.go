129a26eb80f82b30fe633878c4b1391d7028716b
nielwyn <nghoangnghiabt@gmail.com>
2026-04-19 15:32:34 +0000

    add http responses server

---
+++ internal/server/server.go
@@ -0,0 +1,51 @@
+package server
+
+import (
+	"fmt"
+	"net"
+	"sync/atomic"
+)
+
+type Server struct {
+	listener net.Listener
+	isClosed atomic.Bool
+}
+
+func Serve(port int) (*Server, error) {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return nil, err
+	}
+
+	s := &Server{listener: listener}
+	go s.listen()
+	return s, nil
+}
+
+func (s *Server) Close() error {
+	err := s.listener.Close()
+	if err != nil {
+		return err
+	}
+
+	s.isClosed.Store(true)
+	return nil
+}
+
+func (s *Server) listen() {
+	for {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			if s.isClosed.Load() {
+				return
+			}
+			continue
+		}
+		go s.handle(conn)
+	}
+}
+
+func (s *Server) handle(conn net.Conn) {
+	defer conn.Close()
+	conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nHello World!\n"))
+}
