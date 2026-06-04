2a4f6ed779e3c1a38f8b32fb5dc0590654419f35
nielwyn <nghoangnghiabt@gmail.com>
2026-06-05 00:06:03 +0700

    add video handler that serves vim.mp4 with content-type video/mp4

--- cmd/httpserver/handler_video.go
+++ cmd/httpserver/handler_video.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"httpfromtcp/internal/response"
+	"os"
 )
 
 func handleVideo(w *response.Writer) {
-	// TODO: implement video handler
+	data, err := os.ReadFile("assets/vim.mp4")
+	if err != nil {
+		w.WriteStatusLine(response.StatusCodeInternalServerError)
+		w.WriteHeaders(response.GetDefaultHeaders(0))
+		w.WriteBody([]byte(err.Error()))
+		return
+	}
+	h := response.GetDefaultHeaders(len(data))
+	h.Override("content-type", "video/mp4")
+	w.WriteStatusLine(response.StatusCodeOK)
+	w.WriteHeaders(h)
+	w.WriteBody(data)
 }
